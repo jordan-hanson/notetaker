dfbfc15cb182863d146e83c952a624ab009fa90b
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-04-05 21:15:11 -0600

    added html route for no matching route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,9 +7,9 @@ module.exports = function (app) {
     });
 
     // SHOULD IT BE / OR A * FOR HOME PAGE
-    // app.get("*", function(req, res){
-    //     res.sendFile(path.join(_dirname, "./public/index.html"));
-    // });
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/index.html"));
+    });
 
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
